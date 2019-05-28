2210b1a3a7965afbd5258f9ced3a158d31d417a0
unknown <marcela.hdez.1801@gmail.com>
2019-05-27 21:22:56 -0500

    Updates

---
+++ precache-manifest.eeedce7519727397a67fa9d2c4bb8e1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34557d7388e441687decd9a4a663248",
+    "url": "/gdl002-burger-queen/index.html"
+  },
+  {
+    "revision": "0c41048d26c26b4b1a04",
+    "url": "/gdl002-burger-queen/static/css/main.bdf2ca0c.chunk.css"
+  },
+  {
+    "revision": "ab600df400b589bb7dbc",
+    "url": "/gdl002-burger-queen/static/js/2.88ef72e9.chunk.js"
+  },
+  {
+    "revision": "0c41048d26c26b4b1a04",
+    "url": "/gdl002-burger-queen/static/js/main.223dab18.chunk.js"
+  },
+  {
+    "revision": "abf1e5c4314e6441e9d2",
+    "url": "/gdl002-burger-queen/static/js/runtime~main.e107886b.js"
+  }
+]);
